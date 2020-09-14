a69b46ddfd3d4c88d7414ea4df605bc0c6eab470
SFGibby <samgibson124@gmail.com>
2020-09-14 00:52:26 -0600

    Need help with callback #7 but other than that I am pretty solid

--- callbacks/practice.js
+++ callbacks/practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first (arr, callback) {
+  return callback (arr[0]);
+} 
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +49,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(arr, callback) {
+  return callback (arr[arr.length - 1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +69,10 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1,num2,callback){
+  return callback (num1 * num2)
+}
+
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +91,11 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains (arr, name, callback){
+  callback(arr.includes(name))
+    
+}
+
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,8 +116,18 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+function uniq (arr, callback){
+  for (let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[i] === arr[j]){
+        arr.splice(j, 1)
+      }
+    }
+  }
+  return callback(arr);
+}
 
+uniq(testArr)
 // Do not edit the code below.
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
@@ -122,7 +143,11 @@ uniq(names, function(uniqArr){
   For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
 */
 
-//Code Here 
+function each(arrNames, cb){
+  for (let i = 0; i < arrNames.length; i++){
+    cb(arrNames[i], i)
+  }
+} 
 
 // Do not edit the code below.
 each(names, function(item, indice){
@@ -139,7 +164,13 @@ each(names, function(item, indice){
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
-// Code here
+// function getUserById(array, id, callback){
+//   for (let key in getUserById){
+//     if (key === id) {
+//       callback (key);
+//     } 
+//   }
+// }
 
 // Do not edit the code below.
 var users = [
--- objects/practice.js
+++ objects/practice.js
@@ -14,8 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
-
+const me = {
+  name: 'Samuel Gibson',
+  age: 23
+}
+alert (me.name) 
 ////////// PROBLEM 2 //////////
 
 /*
@@ -23,21 +26,29 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: 'Panic! At the Disco',
+  food: 'Hawaiian',
+  person: 'my father',
+  book: 'Ready Player One',
+  movie: 'Baby Driver',
+  holiday: 'Christmas'
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
-
-//Code here
+favoriteThings.car = 'Jeep';
+favoriteThings.brand = 'Patagonia';
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 ////////// PROBLEM 3 //////////
 
@@ -49,13 +60,14 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
 
-//Code here
-
+let backPack = {};
+var item = 'firstPocket'
+backPack.firstPocket = 'chapstick';
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+//alert (backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +75,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +96,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith';
+user2.email = 'bryan.smith@devmounta.in'
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
